b7263f082eb223019cbe58be3116ebbdbecfa14c
ujihisa <ujihisa@gmail.com>
2010-02-01 13:04:57 +0900

    Don't use exec

--- lib/tasks.rb
+++ lib/tasks.rb
@@ -1,8 +1,28 @@
 module Tasks
   module_function
 
-  def spawn(*cmd)
-    fork { exec(*cmd) }
+  # Tasks.spawn
+  # spawns a new process.
+  #
+  # This code is almost equivalent to the following code:
+  #
+  #     def spawn(*cmd)
+  #       fork { exec(*cmd) }
+  #     end
+  #
+  # But Tasks.spawn doesn't use exec() for avoiding making new process.
+  #
+  # e.g. spawn('sleep 100 | echo') generates the following two processes:
+  # * sh -c 'sleep 100 | echo'
+  # * sleep 10
+  def spawn(cmd)
+    a = IO.popen(cmd)
+    fork do
+      while b = a.gets
+        puts b
+      end
+    end
+    a.pid
   end
 
   def love_pact(master, slave, interval = 2)
